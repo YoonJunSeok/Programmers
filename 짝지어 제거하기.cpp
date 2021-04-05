f1bcea669cc3fd6c1964883b71c119937e4eea00
JunSeok Yoon <74297322+YoonJunSeok@users.noreply.github.com>
2021-04-05 10:08:32 +0900

    [Programmers Level.2] 2017 팁스타운. 짝지어 제거하기. 1

---
+++ 짝지어 제거하기.cpp
@@ -0,0 +1,13 @@
+int solution(string s)
+{
+    int answer = 0;
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++) {
+        if (st.empty()) st.push(s[i]);
+        else if (st.top() == s[i]) st.pop();
+        else st.push(s[i]);
+    }
+    if (st.empty())
+        answer = 1;
+    return answer;
+}
